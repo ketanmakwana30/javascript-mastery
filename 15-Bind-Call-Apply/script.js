82f74610b48a1b2d0a3ca00129415357bb79dff2
ketanmakwana30 <ketanmakwana0130@gmail.com>
2026-06-16 23:25:50 +0530

    add some changes

---
+++ 15-Bind-Call-Apply/script.js
@@ -0,0 +1,33 @@
+let obj = {
+    name: 'ketan',
+    age: 19,
+}
+
+
+// Use of Call  --> this is object in this case
+function showName() {
+    console.log(this);
+}
+showName.call(obj);
+
+
+// Use of Call with parameters & Arguments
+function showName2(a, b, c) {
+    console.log(this, a, b, c);
+}
+showName2.call(obj, 1, 2, 3);
+
+
+// Use of Apply --> only 2 Arguments are allowed
+function showName3(a, b, c) {
+    console.log(this, a, b, c);
+}
+showName3.apply(obj, [1, 2, 3]);
+
+
+// Use of Bind --> Create brand-new Function (It store in variable)
+function showName3(a, b, c) {
+    console.log(this, a, b, c);
+}
+let func = showName3.bind(obj, 1, 2, 3);
+func();
