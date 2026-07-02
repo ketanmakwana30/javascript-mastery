c129ac66d4aa77db00aeb24c31abef65792bf62f
ketanmakwana30 <ketanmakwana0130@gmail.com>
2026-07-02 23:40:51 +0530

    modefy funciton

--- 05-Function-practice/script.js
+++ 05-Function-practice/script.js
@@ -8,8 +8,6 @@
 
 // console.log(score(20, 54, 74, 34, 65 , 75, 34, 68, 34, 56, 34, 24, 57 , 56, 76, 53));
 
-
-
 // let other = (val) => {
 //     val();
 // }
@@ -17,16 +15,15 @@
 //     console.log("hellow worlds");
 // });
 
-
 //Closure
 
-function outer () {
-    let a = 10;
-     function inner () {
-        let b = 20;
-        console.log(a  + b);
-    }
-    return inner;
+function outer() {
+  let a = 10;
+  function inner() {
+    let b = 20;
+    console.log(a + b);
+  }
+  return inner;
 }
 
 let innerfun = outer();
