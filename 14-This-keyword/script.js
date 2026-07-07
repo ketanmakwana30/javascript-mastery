fbf39119da77ebeab5ad12ceefd195a1caf2d483
ketanmakwana30 <ketanmakwana0130@gmail.com>
2026-07-07 23:36:25 +0530

    formet

--- 14-This-keyword/script.js
+++ 14-This-keyword/script.js
@@ -1,77 +1,75 @@
-// Global 
-console.log(this);                      // window
-
+// Global
+console.log(this); // window
 
 // Function
-function abcd () {                      // window
-    console.log(this);
+function abcd() {
+  // window
+  console.log(this);
 }
 abcd();
 
-
 // Method of Object (With ES5 function)
-let obj = {                             // object
-    name: "Ketan", 
-    age: 19,
-    sayName: function (){
+let obj = {
+  // object
+  name: "Ketan",
+  age: 19,
+  sayName: function () {
     console.log(this);
-    }
-}
-obj.sayName()
-
+  },
+};
+obj.sayName();
 
 // Method of Object (With ES6 Arrow function)
-let obj2 = {                            // window
-    name: "Ketan", 
-    age: 19,
-    sayName: () => {
-        console.log(this);
-    }
-}
+let obj2 = {
+  // window
+  name: "Ketan",
+  age: 19,
+  sayName: () => {
+    console.log(this);
+  },
+};
 obj2.sayName();
 
-
 // Method of Object (With ES5 inner ES5  function)
-let obj3 = {                            // window
-    name: "Ketan", 
-    age: 19,
-    sayName:function () {
-        function inner (){
-            console.log(this)
-        }
-        inner()
+let obj3 = {
+  // window
+  name: "Ketan",
+  age: 19,
+  sayName: function () {
+    function inner() {
+      console.log(this);
     }
-}
+    inner();
+  },
+};
 obj3.sayName();
 
 // Method of Object (With ES5 inner ES6 Arrow  function)
-let obj4 = {                            // Object
-    name: "Ketan", 
-    age: 19,
-    sayName:function () {
-        let inner = () => {
-            console.log(this)
-        }
-        inner()
-    }
-}
+let obj4 = {
+  // Object
+  name: "Ketan",
+  age: 19,
+  sayName: function () {
+    let inner = () => {
+      console.log(this);
+    };
+    inner();
+  },
+};
 obj4.sayName();
 
-
 // Event Handler
-document.querySelector("h2")
-.addEventListener("click", function() {     // element
-    console.log(this)
-}) 
-
-
+document.querySelector("h2").addEventListener("click", function () {
+  // element
+  console.log(this);
+});
 
 // Classes
 class Student {
-    constructor() {
-        console.log("This is Class !")
-        this.a = 20;                        // Blank Object 
-    }
+  constructor() {
+    console.log("This is Class !");
+    this.a = 20; // Blank Object
+  }
 }
-let createClass = new Student()
+let createClass = new Student();
 console.log(createClass);
