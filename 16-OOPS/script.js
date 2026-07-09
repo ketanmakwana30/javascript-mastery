63db02751f40178f6a2ea4981582b5c654bba601
ketanmakwana30 <ketanmakwana0130@gmail.com>
2026-07-09 21:45:39 +0530

    format oops

--- 16-OOPS/script.js
+++ 16-OOPS/script.js
@@ -1,22 +1,31 @@
 // When we create a Constructor function first letter was Capitle
 
+// This is constructor function
 function CreateCar(name, color, price, mxSpeed, average) {
-    this.name = name;
-    this.color = color;
-    this.price = price;
-    this.mxSpeed = mxSpeed;
-    this.average = average;
-    
-};
+  this.name = name;
+  this.color = color;
+  this.price = price;
+  this.mxSpeed = mxSpeed;
+  this.average = average;
+  // this.write = function () {
+  //   let h1 = document.createElement("h1");
+  //   h1.style.color = this.color;
+  //   h1.textContent = `This is ${this.name}. It's Max speed is ${this.mxSpeed}`;
+  //   document.body.appendChild(h1);
+  // };
+}
 
 // Use prototype
 
-CreateCar.prototype.write = function(text){
-    let h1 = document.createElement("h1")
-    h1.style.color = this.color;
-    h1.textContent = `This is ${this.name}. It's Max speed is ${this.mxSpeed}`;
-    document.body.appendChild(h1)
-}
+CreateCar.prototype.company = "Toyota";
 
-let car1 = new CreateCar("Bugadi", "red", "$50000", 400, 14);
-let car2 = new CreateCar("Fortuner", "gray", "$65000", 500, 20);                          
+CreateCar.prototype.write = function () {
+  let h1 = document.createElement("h1");
+  h1.style.color = this.color;
+  h1.textContent = `This is ${this.name}. It's Max speed is ${this.mxSpeed}`;
+  document.body.appendChild(h1);
+};
+
+// Create Object
+let car1 = new CreateCar("innova", "gray", "$50000", 400, 14);
+let car2 = new CreateCar("Fortuner", "wheat", "$65000", 500, 20);
