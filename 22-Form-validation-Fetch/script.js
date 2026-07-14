0a8f408d715c859a9ade29e427e70b8f29d98314
ketanmakwana30 <ketanmakwana0130@gmail.com>
2026-07-14 18:01:10 +0530

    remove extra code

--- 22-Form-validation-Fetch/script.js
+++ 22-Form-validation-Fetch/script.js
@@ -1,12 +1,7 @@
 const form = document.querySelector("#signupForm");
-
 const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
-
-// const nameError = document.querySelector("#nameError");
-// const emailError = document.querySelector("#emailError");
-// const passwordError = document.querySelector("#passwordError");
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
